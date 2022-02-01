ee2422dad2e377c248a03135fc90bccfb2ec2cf3
mukas <57547835+muquifuler@users.noreply.github.com>
2022-02-01 03:58:38 +0100

    Update send_posts.php

--- PHP/send_posts.php
+++ PHP/send_posts.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     
-    $mysqli = new mysqli('localhost','u505721908_muquifuler','SkDoL:&M;2*','u505721908_autotoken');
+    $mysqli = new mysqli('$SERVER','$USER','$PASS','$BD');
     $wallet_complete = $_SESSION['wallet_complete'];
     $query2 = $mysqli -> query ($sql2 = "SELECT `posts` FROM `Users` WHERE address = \"$wallet_complete\";");
     $valores2 = mysqli_fetch_array($query2);
@@ -11,7 +11,7 @@
     $valores9 = mysqli_fetch_array($query9);
 
 
-    $conexion=mysqli_connect('localhost','u505721908_muquifuler','SkDoL:&M;2*','u505721908_autotoken') or die ('Error en la conexion');  
+    $conexion=mysqli_connect('$SERVER','$USER','$PASS','$BD') or die ('Error en la conexion');  
     mysqli_select_db($conexion,'u505721908_autotoken')or die("problemas al conectar con la base de datos");
     
     if(isset($_POST['title']) && isset($_POST['description']) && isset($_POST['tags']) && isset($_POST['category'])){
